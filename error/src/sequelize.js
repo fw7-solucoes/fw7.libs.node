6f20dda35d2e208e5b8dc6b2197c282ad1246e1f
leonardohabitzreuter <leo.habitzreuter@gmail.com>
2019-11-20 14:28:14 -0300

    updated sequelize handler

--- error/src/sequelize.js
+++ error/src/sequelize.js
@@ -1,18 +1,21 @@
 import R from 'ramda'
-import { fieldError, throwErr, DEFAULT_ERROR } from './common'
+import { schemaError, throwErr, DEFAULT_ERROR } from './common'
 
 const errorEq = R.propEq('validatorKey')
-const getError = R.cond([
-  [errorEq('not_unique'), ({ path, value }) => fieldError(path, `${value} já foi utilizado.`)],
-  [R.T, R.always(DEFAULT_ERROR)]
-])
+const getError = R.pipe(
+  R.head,
+  R.cond([
+    [errorEq('not_unique'), ({ path, value }) => schemaError(R.fromPairs([path, `${value} já foi utilizado.`]))],
+    [R.T, R.always(DEFAULT_ERROR)]
+  ])
+)
 
 const handleDBError = R.pipe(
   R.prop('errors'),
   R.ifElse(
     R.isNil,
-    R.always([DEFAULT_ERROR]),
-    R.map(getError)
+    R.always(DEFAULT_ERROR),
+    getError
   ),
   throwErr
 )
