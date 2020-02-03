b8ab7a4bff8706ad28a626f70d6ab2044b8fb07c
leonardohabitzreuter <leo.habitzreuter@gmail.com>
2020-02-03 15:02:32 -0300

    export types and getJson function

--- messaging/src/index.ts
+++ messaging/src/index.ts
@@ -1,8 +1,14 @@
-import amqp from 'amqplib'
+import amqp, { Message } from 'amqplib'
 import bindConsumers from './consumer'
 import bindPublishers from './publisher'
 import { Consumer, ConnectionConfig } from './types'
 import { Option, some, none, isNone } from 'fp-ts/lib/Option'
+
+export function getJsonContent<T>(msg: Message | null) {
+  return !msg
+    ? none
+    : some(JSON.parse(msg.content.toString()) as T)
+}
 
 async function start<T extends object>(cfg: ConnectionConfig, publish: Option<T>, consume: Consumer[]) {
   const open = await amqp.connect(cfg.host || 'localhost')
@@ -14,4 +20,5 @@ async function start<T extends object>(cfg: ConnectionConfig, publish: Option<T>
   return isNone(publish) ? none : some(bindPublishers(channel, publish.value))
 }
 
+export * from './types'
 export default start
